9d92ba68992bc16d916291c775df402522b13793
Danna Bohnhoff <dannab101@gmail.com>
2022-12-11 06:24:20 -0500

    Adding home page

--- views/default.jsx
+++ views/default.jsx
@@ -1,5 +1,16 @@
 const React = require('react')
 
-function Def(html) {}
+function Def(html) {
+    return (
+        <htmt>
+            <head>
+                <title>Title</title>
+            </head>
+            <body>
+                {html.children}
+            </body>
+        </htmt>
+    )
+}
 
 module.exports = Def
