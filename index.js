b6dab669191277212926be2a1c47b0ca9fda88d6
Danna Bohnhoff <dannab101@gmail.com>
2022-12-10 20:31:10 -0500

    Adding 404 page route

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const app = express()
+require('dotenv').config()
+
 
 app.get('/', (req, res) => {
     res.send('Hello world!')
 })
-// 
-app.listen(8080)
+
+app.get('*', (req, res) => {
+    res.send('<h1>404 Page</h1>')
+})
+
+const PORT = process.env.PORT || 8080
+
+app.listen(PORT, console.log (`listening on port ${PORT}`))
