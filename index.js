4156e5001422d89636935c0c6776c0a10583f485
Danna Bohnhoff <dannab101@gmail.com>
2022-12-11 08:10:30 -0500

    Adding data

--- index.js
+++ index.js
@@ -11,6 +11,23 @@ app.use('/places', require('./controllers/places'))
 app.get('/', (req, res) => {
     res.render('home')
 })
+// GET /places
+app.get('/', (req, res) => {
+    let places = [ {
+        name: 'Joeys Pancake House',
+        city: 'Maggie Valley',
+        state: 'NC',
+        cuisines: 'Breakfast food',
+        pic: 'https://plus.unsplash.com/premium_photo-1663840277618-8eab17e124a3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fHBhbmNha2VzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60'
+    }, {
+        name: 'Southern Porch',
+        city: 'Canton',
+        state: 'NC',
+        cuisines: 'Southern Comfort Food',
+        pic: 'https://media.istockphoto.com/id/1301405497/photo/bbq-lollipop-chicken-wings-and-spicy-glazed-pork-ribs.jpg?b=1&s=170667a&w=0&k=20&c=XZ9qeqVsEJGCVnZLoypy3wGNC_4eEVSdLhX0-U4k1J8='
+       }    ]
+    res.render('places/index', { places })
+})
 
 app.get('*', (req, res) => {
     res.render('error404')
